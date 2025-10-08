ad73e2fa168ebdbd103553cb542b3a42eb6f124e
GeekyGooble <tjh.tanjunyao@gmail.com>
2025-10-08 22:10:54 +0800

    Fix package ordering

--- src/main/java/seedu/address/logic/commands/LinkEventCommand.java
+++ src/main/java/seedu/address/logic/commands/LinkEventCommand.java
@@ -6,8 +6,8 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_EVENT_NAME;
 import java.util.List;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.event.Event;
 import seedu.address.model.event.EventName;
