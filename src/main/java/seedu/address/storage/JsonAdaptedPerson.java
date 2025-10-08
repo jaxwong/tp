6bf3eb7f0be1324d2090af767e1e845587c6a802
GeekyGooble <tjh.tanjunyao@gmail.com>
2025-10-08 22:00:55 +0800

    Update JsonAdaptedPersonTest with new constructor

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -10,14 +10,13 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.event.Event;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
-import seedu.address.model.event.Event;
-import seedu.address.storage.JsonAdaptedEvent;
 
 /**
  * Jackson-friendly version of {@link Person}.
