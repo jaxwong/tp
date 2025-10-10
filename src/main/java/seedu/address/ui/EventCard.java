a127dae677bdc93a4ed96fbe335a963100481adb
jaxwong <wongzhengwei2003@gmail.com>
2025-10-10 11:54:56 +0800

    Add event panels with similar css

---
+++ src/main/java/seedu/address/ui/EventCard.java
@@ -0,0 +1,46 @@
+package seedu.address.ui;
+
+import java.time.format.DateTimeFormatter;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.event.Event;
+
+/**
+ * An UI component that displays information of an {@code Event}.
+ */
+public class EventCard extends UiPart<Region> {
+
+    private static final String FXML = "EventListCard.fxml";
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("MMM dd, yyyy HH:mm");
+
+    public final Event event;
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label name;
+    @FXML
+    private Label id;
+    @FXML
+    private Label description;
+    @FXML
+    private Label startTime;
+    @FXML
+    private Label endTime;
+
+    /**
+     * Creates a {@code EventCard} with the given {@code Event} and index to display.
+     */
+    public EventCard(Event event, int displayedIndex) {
+        super(FXML);
+        this.event = event;
+        id.setText(displayedIndex + ". ");
+        name.setText(event.getName());
+        description.setText(event.getDescription());
+        startTime.setText("Start: " + event.getStart().format(DATE_TIME_FORMATTER));
+        endTime.setText("End: " + event.getEnd().format(DATE_TIME_FORMATTER));
+    }
+}
