ce11a518079ad1fe336a08a17b53f2ff950b29a6
jaxwong <wongzhengwei2003@gmail.com>
2025-10-17 13:43:25 +0800

    Add tests for list todos, mark and unmark todos

---
+++ src/test/java/seedu/address/logic/commands/MarkTodoCommandTest.java
@@ -0,0 +1,182 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalTodos.getTypicalTodos;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.todo.Todo;
+import seedu.address.ui.DisplayList;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for MarkTodoCommand.
+ */
+public class MarkTodoCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        
+        // Add typical todos to both models
+        List<Todo> typicalTodos = getTypicalTodos();
+        for (Todo todo : typicalTodos) {
+            model.addTodo(todo);
+            expectedModel.addTodo(todo);
+        }
+    }
+
+    @Test
+    public void execute_validIndexUnfilteredList_success() {
+        Todo todoToMark = model.getFilteredTodoList().get(INDEX_FIRST_PERSON.getZeroBased());
+        MarkTodoCommand markTodoCommand = new MarkTodoCommand(INDEX_FIRST_PERSON);
+
+        String expectedMessage = String.format(MarkTodoCommand.MESSAGE_MARK_TODO_SUCCESS,
+                Messages.format(todoToMark.withCompletionStatus(true)));
+
+        expectedModel.setTodo(todoToMark, todoToMark.withCompletionStatus(true));
+
+        assertCommandSuccess(markTodoCommand, model, expectedMessage, expectedModel, DisplayList.TODO);
+    }
+
+    @Test
+    public void execute_invalidIndexUnfilteredList_throwsCommandException() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredTodoList().size() + 1);
+        MarkTodoCommand markTodoCommand = new MarkTodoCommand(outOfBoundIndex);
+
+        assertCommandFailure(markTodoCommand, model, Messages.MESSAGE_INVALID_TODO_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_validIndexFilteredList_success() {
+        // Get the first todo before filtering
+        Todo firstTodo = model.getFilteredTodoList().get(0);
+        
+        // Filter todos to show only the first one
+        model.updateFilteredTodoList(todo -> todo.equals(firstTodo));
+        expectedModel.updateFilteredTodoList(todo -> todo.equals(firstTodo));
+
+        Todo todoToMark = model.getFilteredTodoList().get(INDEX_FIRST_PERSON.getZeroBased());
+        MarkTodoCommand markTodoCommand = new MarkTodoCommand(INDEX_FIRST_PERSON);
+
+        String expectedMessage = String.format(MarkTodoCommand.MESSAGE_MARK_TODO_SUCCESS,
+                Messages.format(todoToMark.withCompletionStatus(true)));
+
+        expectedModel.setTodo(todoToMark, todoToMark.withCompletionStatus(true));
+
+        assertCommandSuccess(markTodoCommand, model, expectedMessage, expectedModel, DisplayList.TODO);
+    }
+
+    @Test
+    public void execute_invalidIndexFilteredList_throwsCommandException() {
+        // Get the first todo before filtering
+        Todo firstTodo = model.getFilteredTodoList().get(0);
+        
+        // Filter todos to show only the first one
+        model.updateFilteredTodoList(todo -> todo.equals(firstTodo));
+
+        Index outOfBoundIndex = INDEX_SECOND_PERSON;
+        // ensures that outOfBoundIndex is still in bounds of address book list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getTodoList().size());
+
+        MarkTodoCommand markTodoCommand = new MarkTodoCommand(outOfBoundIndex);
+
+        assertCommandFailure(markTodoCommand, model, Messages.MESSAGE_INVALID_TODO_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_markAlreadyCompletedTodo_success() {
+        // Get a todo that's already completed
+        Todo completedTodo = getTypicalTodos().stream()
+                .filter(Todo::getIsCompleted)
+                .findFirst()
+                .orElse(null);
+        
+        assertTrue(completedTodo != null, "Should have at least one completed todo in test data");
+        
+        // Find the index of this todo
+        int todoIndex = model.getFilteredTodoList().indexOf(completedTodo);
+        Index index = Index.fromZeroBased(todoIndex);
+        
+        MarkTodoCommand markTodoCommand = new MarkTodoCommand(index);
+
+        String expectedMessage = String.format(MarkTodoCommand.MESSAGE_MARK_TODO_SUCCESS,
+                Messages.format(completedTodo.withCompletionStatus(true)));
+
+        expectedModel.setTodo(completedTodo, completedTodo.withCompletionStatus(true));
+
+        assertCommandSuccess(markTodoCommand, model, expectedMessage, expectedModel, DisplayList.TODO);
+    }
+
+    @Test
+    public void execute_markIncompleteTodo_success() {
+        // Get a todo that's not completed
+        Todo incompleteTodo = getTypicalTodos().stream()
+                .filter(todo -> !todo.getIsCompleted())
+                .findFirst()
+                .orElse(null);
+        
+        assertTrue(incompleteTodo != null, "Should have at least one incomplete todo in test data");
+        
+        // Find the index of this todo
+        int todoIndex = model.getFilteredTodoList().indexOf(incompleteTodo);
+        Index index = Index.fromZeroBased(todoIndex);
+        
+        MarkTodoCommand markTodoCommand = new MarkTodoCommand(index);
+
+        String expectedMessage = String.format(MarkTodoCommand.MESSAGE_MARK_TODO_SUCCESS,
+                Messages.format(incompleteTodo.withCompletionStatus(true)));
+
+        expectedModel.setTodo(incompleteTodo, incompleteTodo.withCompletionStatus(true));
+
+        assertCommandSuccess(markTodoCommand, model, expectedMessage, expectedModel, DisplayList.TODO);
+    }
+
+    @Test
+    public void equals() {
+        MarkTodoCommand markFirstCommand = new MarkTodoCommand(INDEX_FIRST_PERSON);
+        MarkTodoCommand markSecondCommand = new MarkTodoCommand(INDEX_SECOND_PERSON);
+
+        // same object -> returns true
+        assertTrue(markFirstCommand.equals(markFirstCommand));
+
+        // same values -> returns true
+        MarkTodoCommand markFirstCommandCopy = new MarkTodoCommand(INDEX_FIRST_PERSON);
+        assertTrue(markFirstCommand.equals(markFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(markFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(markFirstCommand.equals(null));
+
+        // different todo -> returns false
+        assertFalse(markFirstCommand.equals(markSecondCommand));
+    }
+
+    @Test
+    public void toStringMethod() {
+        Index targetIndex = Index.fromOneBased(1);
+        MarkTodoCommand markTodoCommand = new MarkTodoCommand(targetIndex);
+        String expected = MarkTodoCommand.class.getCanonicalName() + "{targetIndex=" + targetIndex + "}";
+        assertEquals(expected, markTodoCommand.toString());
+    }
+}
---
+++ src/test/java/seedu/address/logic/parser/UnmarkTodoCommandParserTest.java
@@ -0,0 +1,63 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.UnmarkTodoCommand;
+
+/**
+ * As we are only doing white-box testing, our test cases do not cover path variations
+ * outside of the UnmarkTodoCommand code. For example, inputs "1" and "1 abc" take the
+ * same path through the UnmarkTodoCommand, and therefore we test only one of them.
+ * The path variation for those two cases occur inside the ParserUtil, and
+ * therefore should be covered by the ParserUtilTest.
+ */
+public class UnmarkTodoCommandParserTest {
+
+    private UnmarkTodoCommandParser parser = new UnmarkTodoCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsUnmarkTodoCommand() {
+        assertParseSuccess(parser, "1", new UnmarkTodoCommand(INDEX_FIRST_PERSON));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnmarkTodoCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_emptyArgs_throwsParseException() {
+        assertParseFailure(parser, "", String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnmarkTodoCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_whitespaceArgs_throwsParseException() {
+        assertParseFailure(parser, "   ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnmarkTodoCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_zeroIndex_throwsParseException() {
+        assertParseFailure(parser, "0", String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnmarkTodoCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_negativeIndex_throwsParseException() {
+        assertParseFailure(parser, "-1", String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnmarkTodoCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_nonIntegerArgs_throwsParseException() {
+        assertParseFailure(parser, "abc", String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnmarkTodoCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_mixedValidInvalidArgs_throwsParseException() {
+        assertParseFailure(parser, "1 abc", String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnmarkTodoCommand.MESSAGE_USAGE));
+    }
+}
+
