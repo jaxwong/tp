c4de13344e799e928b0eaf2edd389e648cc92bd4
GeekyGooble <tjh.tanjunyao@gmail.com>
2025-10-21 11:05:02 +0800

    Remove unused imports

--- src/test/java/seedu/address/logic/commands/EditTodoCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditTodoCommandTest.java
@@ -18,7 +18,6 @@ import seedu.address.logic.commands.EditTodoCommand.EditTodoDescriptor;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.person.Name;
 import seedu.address.model.todo.Todo;
 import seedu.address.testutil.TodoBuilder;
 
