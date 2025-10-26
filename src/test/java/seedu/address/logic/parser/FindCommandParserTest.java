4eb706a18dc4563b5192eab3bf105562c097ad92
enxi <194766557+enxi-low@users.noreply.github.com>
2025-10-26 20:05:21 +0800

    Add tests for FindCommandParser

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -6,6 +6,7 @@ import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailur
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -17,19 +18,36 @@ public class FindCommandParserTest {
     private FindCommandParser parser = new FindCommandParser();
 
     @Test
-    public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
-    }
-
-    @Test
     public void parse_validArgs_returnsFindCommand() {
         // no leading and trailing whitespaces
         FindCommand expectedFindCommand =
                 new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")));
         assertParseSuccess(parser, " " + PREFIX_NAME + "Alice Bob", expectedFindCommand);
 
-        // multiple whitespaces between keywords
-        assertParseSuccess(parser, " " + PREFIX_NAME + " \n Alice \n \t Bob  \t", expectedFindCommand);
+        // multiple whitespaces between keywords and at preamble
+        assertParseSuccess(parser, "    " + PREFIX_NAME + " \n Alice \n \t Bob  \t", expectedFindCommand);
     }
 
+    @Test
+    public void parse_oneArg_returnsFindCommand() {
+        FindCommand expectedFindCommand = new FindCommand(new NameContainsKeywordsPredicate(List.of("Alice")));
+        assertParseSuccess(parser, " " + PREFIX_NAME + "Alice", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_invalidFormat_throwsParseException() {
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE);
+
+        // empty input
+        assertParseFailure(parser, "     ", expectedMessage);
+
+        // non-empty preamble
+        assertParseFailure(parser, " random preamble " + PREFIX_NAME + "Alice", expectedMessage);
+
+        // missing prefix
+        assertParseFailure(parser, " Alice", expectedMessage);
+
+        // missing name but with prefix
+        assertParseFailure(parser, " " + PREFIX_NAME + "   ", expectedMessage);
+    }
 }
